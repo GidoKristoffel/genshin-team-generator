59a953f0de43b8073f3a4845892c3edc5bb9980e
DmitryR <dmytro.ryabchun@gmail.com>
2023-08-16 22:27:06 +0300

    - filters update

--- src/app/components/character-availability-filter/character-availability-filter.component.ts
+++ src/app/components/character-availability-filter/character-availability-filter.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FilterService } from "../../services/filter.service";
 import { EQuality, IFilterCharacter } from "../../interfaces/members.interface";
 import { environment } from "../../../environments/environment";
+import { distinctUntilChanged } from "rxjs";
 
 @Component({
   selector: 'app-character-availability-filter',
@@ -16,22 +17,28 @@ export class CharacterAvailabilityFilterComponent  implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.characters = this.filterService.getAllCharacters();
+    this.filterService.getFilterMembers()
+      .pipe(distinctUntilChanged())
+      .subscribe((filterMembers: IFilterCharacter[]) => {
+        this.characters = filterMembers;
+    });
   }
 
-  public toggleFilterCharacter(index: number): void {
-    this.characters[index].selected = !this.characters[index].selected;
+  public toggleFilterCharacter(index: number, id: number): void {
+    this.filterService.updateFilterMembers([id], !this.characters[index].selected);
   }
 
   public selectAll(): void {
-    this.characters.forEach((character: IFilterCharacter) => {
-      character.selected = true;
-    });
+    this.filterService.updateFilterMembers(
+      this.characters.map((character: IFilterCharacter) => character.id),
+      true
+    );
   }
 
   public reset(): void {
-    this.characters.forEach((character: IFilterCharacter) => {
-      character.selected = false;
-    });
+    this.filterService.updateFilterMembers(
+      this.characters.map((character: IFilterCharacter) => character.id),
+      false
+    );
   }
 }
--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,23 +1,49 @@
 import { Injectable } from '@angular/core';
-import { EQuality, IFilterCharacter, IMember } from "../interfaces/members.interface";
+import { IFilterCharacter, IMember } from "../interfaces/members.interface";
 import { members } from "../../assets/members";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
-  private allMembers: IMember[] = members;
-  constructor() { }
+  private filterMembers: BehaviorSubject<IFilterCharacter[]> = new BehaviorSubject<IFilterCharacter[]>([]);
+  constructor() {
+    this.initFilterMembers();
+  }
 
-  public getAllCharacters(): IFilterCharacter[] {
-    return this.allMembers.map((member: IMember) => {
-      return {
-        id: member.id,
-        name: member.name,
-        icon: member.icon,
-        quality: member.quality,
-        selected: false,
-      };
+  public getFilterMembers(): BehaviorSubject<IFilterCharacter[]> {
+    return this.filterMembers;
+  }
+
+  public getFilterMembersIds(): number[] {
+    return this.filterMembers.getValue()
+      .filter((member: IFilterCharacter) => member.selected)
+      .map((member: IFilterCharacter) => member.id);
+  }
+
+  private initFilterMembers(): void {
+    this.filterMembers.next(
+      members.map((member: IMember) => {
+        return {
+          id: member.id,
+          name: member.name,
+          icon: member.icon,
+          quality: member.quality,
+          selected: false,
+        };
+      })
+    );
+  }
+
+  public updateFilterMembers(ids: number[], selected: boolean): void {
+    const temp = this.filterMembers.getValue();
+    ids.forEach((id: number) => {
+      const index = this.filterMembers.getValue().findIndex((member: IFilterCharacter) => member.id === id);
+      if (index !== -1) {
+        temp[index].selected = selected;
+      }
     });
+    this.filterMembers.next(temp);
   }
 }
