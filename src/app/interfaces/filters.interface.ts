dbe83db84ad4e4e547a6bbfe38640d6e99ee9289
DmitryR <dmytro.ryabchun@gmail.com>
2023-08-21 10:15:21 +0300

    🆕 Start working on adding a new filter

--- src/app/interfaces/filters.interface.ts
+++ src/app/interfaces/filters.interface.ts
@@ -2,8 +2,10 @@ import { Type } from "@angular/core";
 import {
   CharacterAvailabilityFilterComponent
 } from "../components/character-availability-filter/character-availability-filter.component";
+import { WeaponFilterComponent } from "../components/weapon-filter/weapon-filter.component";
+import { RegionFilterComponent } from "../components/region-filter/region-filter.component";
 
 export interface IFilterTab {
   title: string;
-  content: Type<CharacterAvailabilityFilterComponent> | null;
+  content: Type<CharacterAvailabilityFilterComponent | WeaponFilterComponent | RegionFilterComponent> | null;
 }
