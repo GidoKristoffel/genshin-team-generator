81d873eaa73013a2eb12351a345eb845759b2d8e
DmitryR <dmytro.ryabchun@gmail.com>
2023-08-23 12:00:41 +0300

    🆕 Added logic for filter by region

---
+++ src/assets/filters.ts
@@ -0,0 +1,61 @@
+import { IRegionFilter } from "../app/interfaces/filters.interface";
+
+export const weapons = [
+  {
+    id: 1,
+    src: 'assets/img/weapons/sword.webp',
+    title: 'Sword',
+  }, {
+    id: 2,
+    src: 'assets/img/weapons/claymore.webp',
+    title: 'Claymore',
+  }, {
+    id: 3,
+    src: 'assets/img/weapons/bow.webp',
+    title: 'Bow',
+  }, {
+    id: 4,
+    src: 'assets/img/weapons/catalyst.webp',
+    title: 'Catalyst',
+  }, {
+    id: 5,
+    src: 'assets/img/weapons/polearm.webp',
+    title: 'Polearm',
+  }
+];
+
+export const regions: IRegionFilter[] = [
+  {
+    id: 1,
+    src: '',
+    title: 'Mondstadt'
+  }, {
+    id: 2,
+    src: '',
+    title: 'Liyue'
+  }, {
+    id: 3,
+    src: '',
+    title: 'Inazuma'
+  }, {
+    id: 4,
+    src: '',
+    title: 'Sumeru'
+  }, {
+    id: 5,
+    src: '',
+    title: 'Fontaine'
+  }, {
+    id: 6,
+    src: '',
+    title: 'Natlan'
+  }, {
+    id: 7,
+    src: '',
+    title: 'Snezhnaya'
+  }, {
+    id: 8,
+    src: '',
+    title: 'Other'
+  }
+];
