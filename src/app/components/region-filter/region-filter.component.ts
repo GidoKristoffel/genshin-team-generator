27066864724ab459ccb9f95e06a019efe1d28c84
DmitryR <dmytro.ryabchun@gmail.com>
2023-08-24 17:26:58 +0300

    🔧 Added filter display by region

--- src/app/components/region-filter/region-filter.component.ts
+++ src/app/components/region-filter/region-filter.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RegionFilterService } from "../../services/region-filter.service";
+import { IRegionFilter, IRegionFilterSelect } from "../../interfaces/filters.interface";
 
 @Component({
   selector: 'app-region-filter',
   templateUrl: './region-filter.component.html',
   styleUrls: ['./region-filter.component.scss']
 })
-export class RegionFilterComponent {
+export class RegionFilterComponent implements OnInit {
+  public filters: IRegionFilterSelect[] = [];
 
+  constructor(
+    private regionFilterService: RegionFilterService
+  ) {}
+
+  ngOnInit() {
+    this.filtersInit();
+  }
+
+  private filtersInit(): void {
+    this.filters = this.regionFilterService.get().map((filter: IRegionFilter) => ({...filter, selected: true}));
+  }
+
+  public toggleFilter(id: number, index: number): void {
+    this.filters[index].selected = !this.filters[index].selected;
+
+    if (this.filters[index].selected) {
+      this.regionFilterService.add(id);
+    } else {
+      this.regionFilterService.remove(id);
+    }
+  }
 }
