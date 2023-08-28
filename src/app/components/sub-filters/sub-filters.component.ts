16eff7b77ad9c6ba8e999718aca40d96324f4960
DmitryR <dmytro.ryabchun@gmail.com>
2023-08-28 21:55:46 +0300

    🆕 Added sub filters component

---
+++ src/app/components/sub-filters/sub-filters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-filters',
+  templateUrl: './sub-filters.component.html',
+  styleUrls: ['./sub-filters.component.scss']
+})
+export class SubFiltersComponent {
+
+}
